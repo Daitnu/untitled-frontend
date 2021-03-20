f1d394b9ff8a08c622cbd66df4f904525174be03
Seong-Jaeho <jaeho.seong@lighthouse.love>
2021-03-20 15:09:16 +0900

    env: 프로젝트 환경설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,52 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: '/src/index.ts',
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
+
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+      Assets: path.resolve(__dirname, 'assets'),
+    },
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+          'eslint-loader',
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'file-loader',
+        options: {
+          publicPath: '/',
+        },
+      },
+    ],
+  },
+
+  devtool: 'inline-source-map',
 
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
@@ -16,5 +55,14 @@ module.exports = {
     open: true,
     hot: true,
   },
-  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: 'public/index.html' }),
+    new ForkTsCheckerWebpackPlugin({
+      eslint: {
+        files: './src/**/*.{ts,tsx,js,jsx}',
+      },
+    }),
+  ],
 };
