f3e8ded6ab4e342427c0d9c9617ac2592abd3fee
jonghwa <jonghwa0710@naver.com>
2021-04-11 14:54:58 +0900

    footer 수정

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as S from './styled';
 
-const Footer = () => <S.Container>Copyright 2021 홍종화 성재호우. All Rights Reserved.</S.Container>;
+const Footer = () => <S.Container>Copyright Daitnu. All Rights Reserved.</S.Container>;
 
 export default Footer;
