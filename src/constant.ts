9e2abd8c67f4e47782bdccad36a94da5ee5a9f5e
jonghwa <jonghwa0710@naver.com>
2021-04-24 15:51:37 +0900

    COLUMN_NAMES 추가

--- src/constant.ts
+++ src/constant.ts
@@ -6,3 +6,20 @@ export const TOAST_GRID = {
 export const URL = {
   NAVER_FINANCE: 'https://finance.naver.com',
 };
+
+export const COLUMN_NAMES = {
+  MARKET_KIND: 'marketKind', // KOSPI, KOSDAK
+  CORPERATE_NAME: 'corpName', // 회사명
+  TODAY_CLOSE_PRICE: 'todayClosePrice', // 종가
+  CHANGE_PRICE: 'changePrice', // 전일대비 변경금액(전일비)
+  CHANGE_PERCENT: 'changePercent', // 전일대비 변경금액 퍼센트(등락률)
+  CHANGE_PERCENT_WEEK: 'changePercentWeek', // 일주간 변동퍼센트
+  CHANGE_PERCENT_MONTH: 'changePercentMonth', // 한달간 변동퍼센트
+  TODAY_OPEN_PRICE: 'todayOpenPrice', // 금일 시초가
+  TODAY_HIGH_PRICE: 'todayHighPrice', // 금일 최고가
+  TODAY_LOW_PRICE: 'todayLowPrice', // 금일 최저가
+  VOLUME: 'volume', // 거래량
+  SHARES_OUTSTANDING: 'sharesOutstanding', // 발행주식수
+  MARKET_CAPITALIZATION: 'marketCapitalization', // 시가총액
+  NAVER_LINK: 'naverLink', // 네이버바로가기
+};
