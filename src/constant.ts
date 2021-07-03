baaa7f0e7d7d6046047dd9a424514f9296c1b517
jonghwa <jonghwa0710@naver.com>
2021-07-04 00:10:53 +0900

    주간,월간 세달간 등락률 추가

--- src/constant.ts
+++ src/constant.ts
@@ -22,6 +22,9 @@ export const COLUMN_NAMES = {
   SHARES_OUTSTANDING: 'sharesOutstanding', // 발행주식수
   MARKET_CAPITALIZATION: 'marketCapitalization', // 시가총액
   NAVER_LINK: 'naverLink', // 네이버바로가기
+  CHANGE_PERCENT_FOR_A_WEEK : "changePercentForAWeek",
+	CHANGE_PERCENT_FOR_A_MONTH : "changePercentForAMonth",
+	CHANGE_PERCENT_FOR_THREE_MONTH : "changePercentForThreeMonth"
 };
 
 export const PROJECT_NAME = 'Untitled Project';
