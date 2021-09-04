05352a3d22b9d444eca4e973122d0a62fd9fe846
jonghwa <jonghwa0710@naver.com>
2021-09-04 20:17:49 +0900

    update: jpg/png file type

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,3 +1,13 @@
 import { IGridOptions, IStockListData, ITestData } from './data';
 
 export { IGridOptions, IStockListData, ITestData };
+
+declare module '*.jpg' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.png' {
+  const value: any;
+  export = value;
+}
