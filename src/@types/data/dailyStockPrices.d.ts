371912ce7158c7bb88e477b85d79be8a628cc6a2
jonghwa <jonghwa0710@naver.com>
2021-09-04 20:41:49 +0900

    update: stock페이지 type정리

--- src/@types/data/dailyStockPrices.d.ts
+++ src/@types/data/dailyStockPrices.d.ts
@@ -1,5 +1,30 @@
 import { IDailyStockPrice } from '@t/common';
 
+export interface IDailyStockPrice {
+  corpCode: string;
+  corpName: string;
+  marketKind: string;
+  department: string;
+  todayClosePrice: number;
+  changePrice: number;
+  changePercent: number;
+  todayOpenPrice: number;
+  todayHighPrice: number;
+  todayLowPrice: number;
+  volume: number;
+  tradeTotalPrice: number;
+  marketCapitalization: number;
+  sharesOutstanding: number;
+  marketKindId: string;
+  changePercentForAWeek: number;
+  changePercentForAMonth: number;
+  changePercentForThreeMonth: number;
+  rateOfGrowth?: number;
+  year21: number;
+  year22: number;
+  year23: number;
+}
+
 export type IResponseDailyStockPrices = IDailyStockPrice[];
 
 export interface IStockListData extends IDailyStockPrice {
