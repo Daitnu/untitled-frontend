119d8d55d1c582371010168c891c02a6743acf9e
jonghwa <jonghwa0710@naver.com>
2021-09-04 19:44:54 +0900

    update: grid cell border삭제

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -10,29 +10,36 @@ TuiGrid.applyTheme('striped', {
       background: '#e5dbff',
     },
   },
+  cell: {
+    focused: {
+      border: '0',
+    },
+  },
 });
 
-const Grid = ({ data, columns, onClick }) => (
-  <ToastGrid
-    data={data}
-    columns={columns}
-    rowHeight={25}
-    bodyHeight={900}
-    width={'auto'}
-    rowHeaders={[
-      {
-        type: 'rowNum',
-        width: 60,
-      },
-    ]}
-    scrollX={true}
-    scrollY={true}
-    columnOptions={{
-      resizable: true,
-    }}
-    showDummyRows={true}
-    onClick={onClick}
-  />
-);
+const Grid = ({ data, columns, onClick }) => {
+  return (
+    <ToastGrid
+      data={data}
+      columns={columns}
+      rowHeight={25}
+      bodyHeight={900}
+      width={'auto'}
+      rowHeaders={[
+        {
+          type: 'rowNum',
+          width: 60,
+        },
+      ]}
+      scrollX={true}
+      scrollY={true}
+      columnOptions={{
+        resizable: true,
+      }}
+      showDummyRows={true}
+      onClick={onClick}
+    />
+  );
+};
 
 export default Grid;
