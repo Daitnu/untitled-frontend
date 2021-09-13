10d6aebc87e934dcf6de16bebd344c6e35d7b492
jonghwa <jonghwa0710@naver.com>
2021-09-13 18:25:53 +0900

    add: convert 메서드 추가

--- src/libs/convert/convertTo.ts
+++ src/libs/convert/convertTo.ts
@@ -1,11 +1,42 @@
 import { SHILLION, BILLION } from '~/constants';
 
-export const NumberToSeparatorString = (string) => string.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+export const getSeparatorStringFromFormatterValue = (value) => {
+  const number = formatterValueToNumber(value);
+  return NumberToSeparatorString(number);
+};
 
-export const StringToKoreanWon = (string: string): string => {
-  let number = Number(string);
+export const getKoreanWonFromFormatterValue = (value, multiple = 1) => {
+  const number = formatterValueToNumber(value) * multiple;
+  return StringToKoreanWon(number);
+};
+
+export const formatterValueToString = (value): string => {
+  let returnValue = value;
+
+  if (returnValue === null || returnValue === undefined) {
+    returnValue = '';
+  }
+
+  return returnValue.toString();
+};
+
+export const formatterValueToNumber = (value): number => {
+  let returnValue = value;
+
+  if (returnValue === null || returnValue === undefined) {
+    returnValue = 0;
+  }
+
+  return Number(value);
+};
+
+export const NumberToSeparatorString = (value: number | string) =>
+  value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+export const StringToKoreanWon = (value: number | string): string => {
+  let number = Number(value);
   const billionWon = Math.floor(number / BILLION);
   number = number % BILLION;
   const shillionWon = Math.round(number / SHILLION);
-  return billionWon === 0 ? `${shillionWon}억` : `${billionWon}조 ${shillionWon}억`;
+  return billionWon === 0 ? `${shillionWon}억` : `${billionWon}조 ${Math.abs(shillionWon)}억`;
 };
