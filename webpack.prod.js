80d24c18c1ef6abbb4a3014e62357db1382302cf
Seong-Jaeho <jaeho.seong@lighthouse.love>
2021-11-27 13:00:33 +0900

    update(env): webpack dev, prod 분기

---
+++ webpack.prod.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const RobotstxtPlugin = require('robotstxt-webpack-plugin');
+
+const robotsOptions = require('./robots-txt.config');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+      '@t': path.resolve(__dirname, 'src/@types'),
+      '@lib': path.resolve(__dirname, 'src/libraries'),
+      '@const': path.resolve(__dirname, 'src/constants'),
+      Assets: path.resolve(__dirname, 'assets'),
+    },
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+          'eslint-loader',
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          publicPath: '/',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      // {
+      //   test: /\.css$/i,
+      //   loader: 'css-loader',
+      //   options: {
+      //     url: true,
+      //   },
+      // },
+    ],
+  },
+
+  devtool: false,
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: 'public/index.html' }),
+    new ForkTsCheckerWebpackPlugin({
+      eslint: {
+        files: './src/**/*.{ts,tsx,js,jsx}',
+      },
+    }),
+    new RobotstxtPlugin(robotsOptions),
+  ],
+};
