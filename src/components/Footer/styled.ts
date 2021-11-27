c0929baff6c2e3532695efef3a128e7edfe7e00b
Seong-Jaeho <jaeho.seong@lighthouse.love>
2021-11-27 14:14:10 +0900

    update(footer): bottom fixed

--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 import { COLOR } from '~/constants';
 
 export const Container = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
   height: 50px;
   display: flex;
   justify-content: center;
