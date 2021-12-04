e3c3d2135f84259933ed93ba8712c2dd50411d0e
Seong-Jaeho <jaeho.seong@lighthouse.love>
2021-12-04 15:13:40 +0900

    update(register): 템플릿 적용

--- src/pages/Account/Register/Register.tsx
+++ src/pages/Account/Register/Register.tsx
@@ -3,17 +3,20 @@ import * as LS from '../Login/styled';
 import * as GS from '~/globalStyles';
 import { RegisterForm } from '~/components/Forms/Register';
 import backgoundImage from 'Assets/back.jpg';
+import { SimplePageTemplate } from '~/components/templates';
 
 const RegisterPage: React.FC = () => {
   return (
-    <GS.FullScreenWrap bg={backgoundImage}>
-      <LS.CenterWrap>
-        <LS.LoginFormArea>
-          <RegisterForm />
-        </LS.LoginFormArea>
-        <LS.BackgoundImage />
-      </LS.CenterWrap>
-    </GS.FullScreenWrap>
+    <SimplePageTemplate>
+      <GS.FullScreenWrap bg={backgoundImage}>
+        <LS.CenterWrap>
+          <LS.LoginFormArea>
+            <RegisterForm />
+          </LS.LoginFormArea>
+          <LS.BackgoundImage />
+        </LS.CenterWrap>
+      </GS.FullScreenWrap>
+    </SimplePageTemplate>
   );
 };
 
