08b7d406b9e9ed67ffe6ece2f9e52da57b73acb0
Seong-Jaeho <jaeho.seong@lighthouse.love>
2021-12-18 13:22:06 +0900

    update(corperation): 코퍼레이션 변수명 put => patch

--- src/store/corperation/corperationSaga.ts
+++ src/store/corperation/corperationSaga.ts
@@ -3,11 +3,11 @@ import sagaUtils from '~/libraries/sagaUtils';
 import { CORPERATION_PROFIT21_TYPES } from './corperationStore';
 import corperationApi from './CorperationApi';
 
-const corperationProfitPut$ = sagaUtils.makeApiCallSagaFunc({
+const corperationProfitPatch$ = sagaUtils.makeApiCallSagaFunc({
   type: CORPERATION_PROFIT21_TYPES.DEFAULT,
   apiFunc: corperationApi.updateCoperationProfit,
 });
 
-export function* corperationProfitPutSaga() {
-  yield all([takeEvery(CORPERATION_PROFIT21_TYPES.REQUEST, corperationProfitPut$)]);
+export function* corperationProfitPatchSaga() {
+  yield all([takeEvery(CORPERATION_PROFIT21_TYPES.REQUEST, corperationProfitPatch$)]);
 }
