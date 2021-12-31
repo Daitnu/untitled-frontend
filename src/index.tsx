0f5222bc6d1b150f46dcee278885546ad334fd25
jonghwa <jonghwa0710@naver.com>
2021-12-31 23:17:26 +0900

    update: I18N contants API_PATH로 대체

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './router';
 import { apiInstance } from '@lib/api';
+import API_PATH from '~/constants/path';
 
 apiInstance
   .get({
-    url: '/static/ko.json',
+    url: API_PATH.I18N_KO,
   })
   .then((result) => {
     console.log('i18n', result);
