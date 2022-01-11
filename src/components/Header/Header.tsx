260d2ccfc50f272b6e3c1136a7b860f3d41884bb
jonghwa <jonghwa0710@naver.com>
2022-01-11 22:51:03 +0900

    update: i18n 파일명 index.ts로변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { PATH_URL } from '~/constants';
 import * as S from './styled';
 import { useTranslation } from 'react-i18next';
-import i18n from '~/i18n';
+import i18n from 'i18next';
 import { Button } from 'antd';
 
 const Header = () => {
