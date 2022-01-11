46009ddc6635e3ca133fd0ea04994af6fdcceb01
jonghwa <jonghwa0710@naver.com>
2022-01-11 22:36:10 +0900

    update: 다국어 토글처리

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { PATH_URL } from '~/constants';
 import * as S from './styled';
+import { useTranslation } from 'react-i18next';
+import i18n from '~/i18n';
+import { Button } from 'antd';
 
 const Header = () => {
+  const { t } = useTranslation();
+
   return (
     <S.Header>
       <S.Container>
@@ -23,8 +28,10 @@ const Header = () => {
           <Link to="/mypage">Mypage</Link>
         </S.flexItem> */}
         <S.flexItem>
-          <Link to={PATH_URL.LOGIN}>Login</Link>
+          <Link to={PATH_URL.LOGIN}>{t('HEADER.LOGIN')}</Link>
         </S.flexItem>
+
+        <Button onClick={() => i18n.changeLanguage(i18n.language === 'en' ? 'ko' : 'en')}>{i18n.language}</Button>
       </S.Container>
     </S.Header>
   );
