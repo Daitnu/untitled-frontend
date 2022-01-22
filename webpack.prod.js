a150876da981d701dd15f639ea8f0f8409720371
Seong-Jaeho <jaeho.seong@lighthouse.love>
2022-01-22 09:42:53 +0900

    update filename

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.tsx',
   output: {
-    filename: 'bundle.js',
+    filename: '[contenthash].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
   },
