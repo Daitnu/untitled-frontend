6b89d2aa9390dfa80a9186dc34acf2885c8396d9
Seong-Jaeho <jaeho.seong@lighthouse.love>
2022-03-05 12:03:49 +0900

    update(logout): path 변경 및 구현 완료

--- src/pages/Account/Logout/Logout.tsx
+++ src/pages/Account/Logout/Logout.tsx
@@ -6,15 +6,18 @@ import { apiInstance, history } from '~/libraries/api';
 
 const LogoutPage: React.FC = () => {
   useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    const grantType = localStorage.getItem('grantType');
+    const authorizationHeader = accessToken ? `${grantType} ${accessToken}` : '';
     apiInstance
-      .get({ url: API_PATH.ACCOUNT.LOGOUT })
-      .then(() => {
+      .get({ url: API_PATH.ACCOUNT.LOGOUT, token: authorizationHeader })
+      .catch(() => {
+        message.error('서버에러입니다.', 10);
+      })
+      .finally(() => {
         localStorage.removeItem('accessToken');
         localStorage.removeItem('grantType');
         history.push(PATH_URL.LOGIN);
-      })
-      .catch(() => {
-        message.error('서버에러입니다.', 10);
       });
   }, []);
 
